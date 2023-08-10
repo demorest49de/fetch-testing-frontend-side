041928863e8769c69dd4d519475ac845942aa5e6
Shevchenko Andrey <demorest49de@gmail.com>
2023-08-10 22:26:26 +0300

    Changes index.html index.js styles.css

--- index.js
+++ index.js
@@ -1,43 +1,112 @@
 "use strict";
 
-//fetch('db.json');
-// fetch('http://localhost:3000/api/goods', {
-//     method: 'POST',
-//     body: JSON.stringify({
-//         title: 'mango',
-//         description: 'mango is tropical fruit',
-//         category: 'fruit',
-//         price: 50,
-//         units: 'piece',
-//         count: 30
-//     }),
-//     headers: {
-//         'Content-Type': 'application/json'
-//     }
-// });
+const URL = 'https://jsonplaceholder.typicode.com/posts';
+const postWrapper = document.querySelector('.post-wrapper');
 
-
-const loadGoods = async (cb) => {
-    const result = await fetch('http://localhost:3000/api/goods');
-    const data = await result.json();
-    
-    console.log(' : ', data);
-    cb(data);
-    return data;
+// правильная функция для работы с запросами - универсальная
+const httpRequest = (url, {
+    method = 'GET',
+    callback,
+    body = {},
+    headers,
+}) => {
+    try {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, URL);
+        if (headers) {
+            for (const [key, value] of Object.entries(headers)) {
+                xhr.setRequestHeader(key, value);
+            }
+        }
+        
+        xhr.addEventListener('load', () => {
+            if (xhr.status < 200 || xhr.status >= 300) {
+                callback(new Error(xhr.status), xhr.statusText);
+                return;
+            }
+            const data = xhr.response;
+            console.log(' : ', callback);
+            if (callback) callback(null, data);// здесь происходит вызов рендерГудс
+        });
+        
+        xhr.addEventListener('error', () => {
+            console.log('error',);
+            callback(new Error(xhr.status), xhr.response);
+        });
+        
+        xhr.send(JSON.stringify(body));
+    } catch (error) {
+        callback(new Error(error));
+    }
 };
 
-const renderGoods = (data) => {
+const cbGET = (error, data) => {
+    if (error) {
+        const status = data;
+        const checkedStatus = status ? `, ${status}` : '';
+        console.warn(' что-то пошло не так: ', error,checkedStatus);
+        alert(` что-то пошло не так: ${error}${checkedStatus}`);
+        return;
+    }
     
-    const cardwrapper = document.createElement('div');
-    cardwrapper.classList.add('cards');
-    const goods = data.map(item => {
-        console.log(' : ', item);
+    const posts = JSON.parse(data).map(x => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.innerHTML = `
+           <h2>Title: ${x.title}</h2>
+           <p>Text: ${x.body}</p>
+        `;
+        card.style.cssText = `
+           border: 1px solid black;
+           max-width: 250px;
+        `;
+        return card;
     });
-    
-    cardwrapper.append(...goods);
+    console.log(posts);
+    postWrapper.style.cssText = `
+        margin: 0 auto;
+        display:flex;
+        flexDirection: row;
+        flex-wrap: wrap;
+    `;
+    postWrapper.append(...posts);
 };
 
+//GET
+const getBtn = document.querySelector('#get');
+getBtn.addEventListener('click', () => {
+    httpRequest(URL, {
+        method: 'GET',
+        callback: cbGET,
+    });
+});
 
-// console.log(' : ', loadGoods());
-const goods = loadGoods(renderGoods);
-console.log(' : ', goods);
+
+//POST
+const form = document.querySelector('#form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();//что бы страница не перезагружалась от действий по умолчанию;
+    
+    httpRequest(URL, {
+        method: 'POST',
+        callback(error, data) {
+            if (error) {
+                const status = data;
+                const checkedStatus = status ? `, ${status}` : '';
+                console.warn(' что-то пошло не так: ', error,checkedStatus);
+                alert(` что-то пошло не так: ${error}${checkedStatus}`);
+                form.textContent = ` что-то пошло не так: ${error}`;
+            } else {
+                console.log(' data from POST method:) ', data);
+                form.textContent = `заявка принята. ваш номер ${JSON.parse(data).id}`;
+            }
+        },
+        body: {
+            title: form.title.value,
+            body: form.description.value
+        },
+        headers: {
+            'Content-type': 'application/json',
+        }
+    });
+});
